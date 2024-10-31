7b08e325abf034e6c5108374c8b28a2f5907aa1a
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-10-31 21:29:57 +0200

    add header & fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "./page.module.css";
+import MainContent from "@/components/MainContent";
 
 export default function HomePage() {
   return (
     <div className={styles.page}>
+      <MainContent />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
+import React from "react";
 
-export default function Header() {
+const Header: React.FC = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <AppBar
+      position="sticky"
+      color="inherit"
+      sx={{ boxShadow: "none", borderBottom: "1px solid #e0e0e0" }}
+    >
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "30px 20px",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{ color: "#000000", fontWeight: "bold", fontSize: "1.5rem" }}
+        >
+          Some Company
+        </Typography>
+        <Link href="/contact">
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#333",
+              color: "#fff",
+              textTransform: "none",
+              fontWeight: "300",
+              fontSize: "1rem",
+              borderRadius: "7px",
+              padding: "5px 50px",
+              "&:hover": {
+                backgroundColor: "#555",
+              },
+            }}
+          >
+            Contact us
+          </Button>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
