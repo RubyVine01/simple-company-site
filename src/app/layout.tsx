112c4e4f924282531b23eafe5e6fd8ee57d1bce3
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-11-02 22:08:38 +0200

    add Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,37 @@ import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: "Simple Company Site",
   description: "Simple Company Site",
+  openGraph: {
+    title: "Simple Company Site",
+    description: "A simple site demonstrating React and Next.js.",
+    type: "website",
+    url: "https://example.com",
+    images: [
+      {
+        url: "https://example.com/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Simple Company Site",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@example",
+    title: "Simple Company Site",
+    description: "A simple site demonstrating React and Next.js.",
+    images: [
+      {
+        url: "https://example.com/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Simple Company Site",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
