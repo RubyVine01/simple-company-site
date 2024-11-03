2f5542dc144da88b41cf5dbca03b3e4282b41fa7
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-11-03 02:54:43 +0200

    type edits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
+import { Roboto } from "next/font/google";
 import type { Metadata } from "next";
 import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Box } from "@mui/material";
 
-const metadata: Metadata = {
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
   title: "Simple Company Site",
   description: "Simple Company Site",
   openGraph: {
@@ -43,13 +50,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" className={roboto.className}>
+      <head></head>
       <body>
         <Header />
         <Box component="main">{children}</Box>
